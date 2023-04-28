709b87291b0b474b085b9e998aeecb6b570b5c8f
Rokas <rookas.rudzenskas@gmail.com>
2023-04-28 20:04:28 +0300

    ✨ feat(tabs/index.tsx): add button to create new tweet and navigate to new-tweet screen ✨ feat(_layout.tsx): add new-tweet screen to navigation stack ✨ feat(new-tweet.tsx): create new-tweet screen with basic layout and UI elements

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,8 +5,11 @@ import {Image, TouchableOpacity, FlatList} from "react-native";
 import {Feather} from "@expo/vector-icons";
 import React from "react";
 import Tweet from "../../components/Tweet";
+import {Link, useRouter} from 'expo-router';
+
 
 export default function TabOneScreen() {
+  const router = useRouter()
   return (
     <View className="h-full">
       <FlatList
@@ -14,6 +17,9 @@ export default function TabOneScreen() {
         showsVerticalScrollIndicator={false}
         renderItem={({item}) => <Tweet tweet={item} />}
       />
+      <TouchableOpacity activeOpacity={0.7} onPress={() => router.push('/new-tweet')} className="absolute bottom-4 flex items-center justify-center right-6 w-16 h-16 rounded-full bg-[#1da1f2]">
+        <Feather name="plus" size={35} color="white" />
+      </TouchableOpacity>
     </View>
   );
 }
---
+++ app/new-tweet.tsx
@@ -0,0 +1,32 @@
+//@ts-nocheck
+import React from 'react';
+import {Text, View, StyleSheet, TouchableOpacity, Image, TextInput} from 'react-native';
+import {Ionicons} from "@expo/vector-icons";
+
+const NewTweet = () => {
+  return (
+    <View className="h-screen bg-white pt-16 px-5">
+      <View className="w-full flex flex-row items-center justify-between">
+        <TouchableOpacity className="" activeOpacity={0.7}>
+          <Text className="text-[15px]">Cancel</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity className="bg-[#1da1f2] px-5 py-2 rounded-full" activeOpacity={0.7}>
+          <Text className="text-[15px] text-white font-bold">Tweet</Text>
+        </TouchableOpacity>
+      </View>
+      <View className="flex flex-row items-center">
+        <Image className="w-10 h-10 rounded-full" source={{uri: 'https://pbs.twimg.com/profile_images/1595750905537871873/DMcn47S6_400x400.jpg'}} />
+        <View className="flex flex-col ml-3">
+          <View className="flex flex-row border mt-2 inline-flex w-[80px] items-center justify-center space-x-1 rounded-full border-2 border-[#1da1f2]">
+            <Text className="text-[#1da1f2] font-semibold">Public</Text>
+            <Ionicons name="chevron-down-sharp" size={18} color="#1da1f2" />
+          </View>
+          {/*<TextArea />*/}
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default NewTweet;
