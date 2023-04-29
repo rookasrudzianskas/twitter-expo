cf31e21cf5be1aebdd992c2a6876d3d418bcf9ee
Rokas <rookas.rudzenskas@gmail.com>
2023-04-29 11:08:11 +0300

    🎨 style(drawer/_layout.tsx): add unstable_settings object to ensure back button is present when reloading on /modal ✨ feat(drawer/_layout.tsx): add Drawer.Screen for (tabs) to DrawerNavigator 🎨 style(_layout.tsx): remove Stack.Screen for (tabs) from RootLayoutNav to be handled by DrawerNavigator ✨ feat(tabs/_layout.tsx): create TabLayout component with two screens, index and two ✨ feat(tabs/index.tsx): create TabOneScreen component with FlatList of tweets and button to navigate to new-tweet screen ✨ feat(tabs/two.tsx): create TabTwoScreen component with title and separator view components

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -4,6 +4,15 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 const DrawerNavigator = createDrawerNavigator().Navigator;
 
 const Drawer = withLayoutContext(DrawerNavigator);
+
+export const unstable_settings = {
+  // Ensure that reloading on `/modal` keeps a back button present.
+  initialRouteName: '(tabs)',
+};
 export default function DrawerLayout() {
-  return <Drawer />
+  return (
+    <Drawer>
+      <Drawer.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Drawer>
+  )
 }
