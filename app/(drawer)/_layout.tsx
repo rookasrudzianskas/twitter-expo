0f4904bd1b34033a858a11fc81158fbcc3a9ca53
Rokas <rookas.rudzenskas@gmail.com>
2023-04-29 11:00:10 +0300

    🚀 feat(drawer): add DrawerNavigator to the app to improve navigation 🔧 chore(babel.config.js): add react-native-reanimated plugin to babel configuration 🔧 chore(index.ts): import react-native-gesture-handler to fix navigation issues 📦 chore(package.json): add @react-navigation/drawer and react-native-reanimated dependencies to the project

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,5 +1,9 @@
-import { Stack } from "expo-router";
+import { Stack, withLayoutContext } from "expo-router";
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
+const DrawerNavigator = createDrawerNavigator().Navigator;
+
+const Drawer = withLayoutContext(DrawerNavigator);
 export default function DrawerLayout() {
-  return <Stack />
+  return <Drawer />
 }
