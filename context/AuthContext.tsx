7e0ae4e61ec2e158376b51f733ddc75d6fd9d74e
Rokas <rookas.rudzenskas@gmail.com>
2023-05-10 09:39:24 +0300

    🔨 refactor(AuthContext.tsx): remove commented out code ✨ feat(AuthContext.tsx): add state and methods to handle authentication token and redirect user based on authentication status 🐛 fix(AuthContext.tsx): fix issue where user was not redirected to sign in page when authToken was null and user was not in auth group

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -12,10 +12,40 @@ const AuthContext = createContext({});
 
 
 const AuthContextProvider = ({ children }: PropsWithChildren) => {
+  const [authToken, setAuthToken] = useState<string | null>(null);
+  const segments = useSegments();
+  const router = useRouter();
+
+  useEffect(() => {
+    const isAuthGroup = segments[0] === '(auth)';
+
+    if (!authToken && !isAuthGroup) {
+      router.replace('/signIn');
+    }
+    if (authToken && isAuthGroup) {
+      router.replace('/');
+    }
+  }, [segments, authToken]);
+
+  useEffect(() => {
+    const loadAuthToken = async () => {
+      const res = await SecureStore.getItemAsync('authToken');
+      if (res) {
+        setAuthToken(res);
+      }
+    };
+    loadAuthToken();
+  }, []);
+
+  const updateAuthToken = async (newToken: string) => {
+    await SecureStore.setItemAsync('authToken', newToken);
+    setAuthToken(newToken);
+  };
+
   return (
     <AuthContext.Provider value={{
-      // authToken,
-      // updateAuthToken
+      authToken,
+      updateAuthToken
     }}>
       {children}
     </AuthContext.Provider>
